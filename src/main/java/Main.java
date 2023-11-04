4e9960cf89aaac13cbe1926732cf49ea4d43c1f2
TrueRaisins <idanik.ishot@gmail.com>
2023-11-04 16:54:08 +0300

    initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        PrimeNumber primeNumber = new PrimeNumber(12);
+        PrimeNumber primeNumber = new PrimeNumber(17);
         primeNumber.calculate();
 
         System.out.println(primeNumber);
--- src/main/java/PrimeNumber.java
+++ src/main/java/PrimeNumber.java
@@ -6,22 +6,22 @@ public class PrimeNumber {
     }
 
     public void calculate() {
-        int number = 1;
+        int k = number;
         boolean prime = true;
-        if (number <= 1) {
+        if (k <= 1) {
             prime = false;
         } else {
-            for (int i = 2; i <= Math.sqrt(number); i++) {
-                if (number % i == 0) {
+            for (int i = 2; i <= Math.sqrt(k); i++) {
+                if (k % i == 0) {
                     prime = false;
                     break;
                 }
             }
         }
         if (prime) {
-            System.out.println(number + " " + "простое число");
+            System.out.println(k + " " + "простое число");
         } else {
-            System.out.println(number + " " + "не является простым числом");
+            System.out.println(k + " " + "не является простым числом");
         }
     }
 }
